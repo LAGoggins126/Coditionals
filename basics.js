d0dca1ca74b5a63bdae60c4515537bc46ffab893
Luka Goggins <lukarowangoggins@computingforall.org>
2021-10-02 12:04:54 -0700

    complete

--- basics.js
+++ basics.js
@@ -5,7 +5,14 @@
 
 if (7 < 10) {
     console.log (true);
-//this wiill only occur if the statement is false
+//this will only occur if the statement is false
+} else {
+    console.log(false);
+}
+
+if (71 < 10) {
+    console.log (true);
+//this will only occur if the statement is false
 } else {
     console.log(false);
 }
@@ -30,30 +37,46 @@ if (x < 10) {
 
 function hobbies() {
 
-    //create a variable for "newHobby"
+//     //create a variable for "newHobby"
     let newHobby;
+    let randomNum = Math.floor(Math.random()* 10);
 
     //Math.random randomly selects a number that fulfills these conditions: 0 < x < 1
     //Math.floor returns the largest integer less than or equal to a given number
-    if (Math.floor(Math.random()* 10) < 3) {
-        newHobby = true;
-        console.log(newHobby);
+    if (randomNum < 3) {
+        newHobby = "yoga";
+        console.log("your new hobby is " + newHobby);
+    } else if (randomNum > 6) {
+        newHobby = "running";
+        console.log("your new hobby is " + newHobby );
     } else {
-        console.log(false);
+        console.log("you don't get a new hobby");
     }
-}
 
+};
 hobbies();
 
 //You want to create a function that randomly decides what movie you will go to see
 //create an if/else if/else statement that utilizes math.random and math.floor to decide between 3 movies
-
+let num = Math.floor(Math.random()* 10);
 //create a function for your movieChoice
-
+movieChoice();
 //create a variable, but do not set it equal to anything
-
+let movie;
 //start your if else statement
 
+function movieChoice(){
+    if (num < 5) {
+    movie = "Tangled";
+    console.log("watch " + movie);
+    } else if (num > 7) {
+    movie = "Lilo and Stitch";
+    console.log("watch " + movie);
+    } else {
+    movie = "World War Z";
+    console.log("watch " + movie);
+    }
+}
 //set your variable = to a movie
 
 //console.log that movie
@@ -62,4 +85,4 @@ hobbies();
 
 //repeat for else
 
-//Don't forget to call your function
+
--- diceGame.js
+++ diceGame.js
@@ -2,19 +2,26 @@
 //create a function called diceGame
 function diceGame() {
     
-    //create a variable called dice1 that will contain a random number from 1-10 
+    //create a variable called dice1 that will con;tain a random number from 1-10 
     //Use Math.floor and Math.random to create this number
-    
+
+    let dice1 = Math.floor(Math.random()*10);
 
     //create a variable called dice2 that will contain a random number from 1-10 
     //Use Math.floor and Math.random to create this number
+    let dice2 = Math.floor(Math.random()*10);
    
-
+    firstRoll();
+    secondRoll();
     //create a function for your first roll
     function firstRoll() {
     
         //start an if then statement that sets conditions for dice1
-        
+        if (dice1 < 6){
+            console.log("winner");
+        } else {
+            console.log("loser");
+        }
         //console.log 'winner' if it meets the conditions
            
         //use the else portion to console log 'loser'
@@ -25,7 +32,11 @@ function diceGame() {
     //create a function for your second roll
     function secondRoll() {
         //start an if then statement that sets conditions for dice2
-        
+        if (dice2 < 6){
+            console.log("winner");
+        } else {
+            console.log("loser");
+        }
             //console.log 'winner' if it meets the conditions
            
             //use the else portion to console log 'loser'
